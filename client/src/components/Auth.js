91b5e8adff5c792e506ba3f941c71791bb4ee084
repsukka <81526112+repsukka@users.noreply.github.com>
2023-11-08 12:15:50 +0200

    Client auth

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,30 +1,44 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { LoginContext } from "./Contexts";
-import { jwtToken } from "./Signals";
+import { jwtToken, userData } from "./Signals";
+import axios from "axios";
 
 function Login() {
-  
-    const buttonLabel = login ? 'Logout' : 'Login';
-  
+
     return (
       <div>
-        {jwtToken.value.length > 0 ? <h2>Welcome!</h2> : <h4>Unauthorized</h4>}
-        <button onClick={() => setLogin(!login)}>
-          {buttonLabel}
-        </button>
+        <UserInfo/>
+        { jwtToken.value.length === 0 ? <LoginForm/> : 
+          <button onClick={() => jwtToken.value = ''}>Logout</button>}
       </div>
     );
   }
 
-  function LoginForm(){
-
-    const [username, setUsername] = useState('');
-    const [pw, setPw] = useState('');
+  function UserInfo(){
 
     return(
       <div>
-        <input value={username} onChange={e => setUsername(e.target.value)}/>
+        {jwtToken.value ? <h1>{userData.value?.private}</h1> : <h1>You are guest</h1>}
+      </div>
+    )
+  }
+
+  function LoginForm(){
+
+    const [uname, setUname] = useState('');
+    const [pw, setPw] = useState('');
+
+    function login(){
+      axios.postForm('http://localhost:3001/user/login', {uname,pw})
+        .then(resp => jwtToken.value = resp.data.jwtToken )
+        .catch(error => console.log(error.response.data))
+    }
+
+    return(
+      <div>
+        <input value={uname} onChange={e => setUname(e.target.value)}/>
         <input value={pw} onChange={e => setPw(e.target.value)}/>
+        <button onClick={login}>Login</button>
       </div>
     );
   }
--- client/src/components/Signals.js
+++ client/src/components/Signals.js
@@ -1,3 +1,21 @@
-import { signal } from "@preact/signals-react";
+import { effect, signal } from "@preact/signals-react";
+import axios from "axios";
 
-export const jwtToken = signal('');
+export const jwtToken = signal(getSessionToken());
+export const userData = signal(null);
+
+function getSessionToken(){
+    const t = sessionStorage.getItem('token');
+    return t===null || t==='null' ? '' : t;
+}
+
+effect(()=>{
+    sessionStorage.setItem('token', jwtToken.value);    
+
+    if(jwtToken.value.length > 0){
+        const config = {headers:{ Authorization: 'Bearer ' + jwtToken.value }};
+        axios.get('http://localhost:3001/user/private', config)
+            .then(resp => userData.value = resp.data)
+            .catch(err => console.log(err.response.data))
+    }
+});
